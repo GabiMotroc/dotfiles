a4803ee2af193f400d1b6c0726ad113eba7b2fd3
gabi <motrocgabi@gmail.com>
2026-05-08 00:29:52 +0300

    refactor

--- setup.sh
+++ setup.sh
@@ -20,60 +20,56 @@ if [ "$PROMPT" = true ]; then
     [[ "$reply" =~ ^[Yy]$ ]] && INSTALL_LAZYDOCKER=true
 fi
 
-echo "Installing zsh, gh, curl, git..."
-sudo apt install -y zsh gh curl git
+install_or_update() {
+    local url=$1 dir=$2 name=$3
+    if [ ! -d "$dir" ]; then
+        echo "Installing $name..."
+        git clone --depth=1 "$url" "$dir"
+    else
+        echo "Updating $name..."
+        git -C "$dir" pull --ff-only
+    fi
+}
+
+apt_install() {
+    local packages=("$@")
+    sudo apt install -y "${packages[@]}"
+}
+
+echo "Installing system packages..."
+apt_install zsh gh curl git
+
+install_or_update https://github.com/romkatv/powerlevel10k.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" powerlevel10k
+
+for plugin in zsh-autosuggestions zsh-syntax-highlighting; do
+    install_or_update "https://github.com/zsh-users/$plugin.git" \
+        "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/$plugin" "$plugin"
+done
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing Oh My Zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-else
-    echo "Oh My Zsh already installed"
 fi
-
-ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
-
-if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
-    echo "Installing powerlevel10k..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
-else
-    echo "Updating powerlevel10k..."
-    git -C "$ZSH_CUSTOM/themes/powerlevel10k" pull --ff-only
-fi
-
-for plugin in zsh-autosuggestions zsh-syntax-highlighting; do
-    if [ ! -d "$ZSH_CUSTOM/plugins/$plugin" ]; then
-        echo "Installing $plugin..."
-        git clone https://github.com/zsh-users/$plugin.git "$ZSH_CUSTOM/plugins/$plugin"
-    else
-        echo "Updating $plugin..."
-        git -C "$ZSH_CUSTOM/plugins/$plugin" pull --ff-only
-    fi
-done
 
 echo "Copying configs..."
-cp alacritty.toml ~/.config/alacritty/alacritty.toml 2>/dev/null || (
-    mkdir -p ~/.config/alacritty && cp alacritty.toml ~/.config/alacritty/alacritty.toml)
+mkdir -p ~/.config/alacritty
+cp alacritty.toml ~/.config/alacritty/alacritty.toml
 cp .zshrc ~/.zshrc
 
 if [ "$INSTALL_LAZYGIT" = true ]; then
     echo "Installing lazygit..."
-    sudo apt install -y lazygit
+    apt_install lazygit
 fi
 
-if [ "$INSTALL_LAZYDOCKER" = true ]; then
-    if command -v lazydocker &>/dev/null; then
-        echo "lazydocker already installed"
-    else
-        echo "Installing lazydocker..."
-        curl -fsSL https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-    fi
+if [ "$INSTALL_LAZYDOCKER" = true ] && ! command -v lazydocker &>/dev/null; then
+    echo "Installing lazydocker..."
+    curl -fsSL https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 fi
 
 if [ "$SHELL" != "$(which zsh)" ]; then
     echo "Setting zsh as default shell..."
-    chsh -s $(which zsh)
-else
-    echo "zsh is already the default shell"
+    chsh -s "$(which zsh)"
 fi
 
 echo "Done! Log out and back in."
